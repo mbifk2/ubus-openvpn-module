8e15eaaa85f93ceed65fa30150b0f9075da85fb5
matbin <@matbin>
2025-08-19 12:22:58 +0300

    Learning ubus methods in Lua

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,11 @@
 local ubus = require("ubus")
 local uloop = require("uloop")
+local json = require("json")
 
 uloop.init()
-local ctx = ubus.connect();
+local conn = ubus.connect();
 
-if not ctx then
+if not conn then
     error("Ubus connection failed")
 end
 
@@ -13,15 +14,31 @@ local ovpn_dir = " /var/run/openvpn"
 local function get_servers()
     local servers = {}
     for file in io.popen('ls -1' .. ovpn_dir):lines() do
-        local s = file:match("^openvpn%-(.+)%.status$")
+        local s = file:match("^openvpn%-(.+)%.info$")
         if s then
             table.insert(servers, s)
-        else
-            print("Ignoring: " .. file)
         end
     end
     return servers
 end
+
+local ovpn_methods = {
+    openvpn = {
+        fake = {
+            function(req)
+            conn:reply(req, {message="hello"});
+            end, {id = "fail"}
+        },
+    },
+    servers = {
+        function(req, msg)
+        conn:reply(req, {message="hello"});
+        print("call to servers")
+        end, {id = ubus.INT32, msg = ubus.STRING}
+    }
+}
+
+conn:add(ovpn_methods)
 
 for _, s in ipairs(get_servers()) do
     local server = ovpn_dir .. "/openvpn-" .. s .. ".status"
