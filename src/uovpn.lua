2eb488e904beb308b51fd2cd5af081392db51032
matbin <@matbin>
2025-08-28 09:10:14 +0300

    Everything in one file

--- src/uovpn.lua
+++ src/uovpn.lua
@@ -4,8 +4,7 @@ local ubus = require("ubus")
 local uloop = require("uloop")
 local uci = require("uci")
 
-local mgmt = require("ubus_openvpn_module.management")
-local clients = require("ubus_openvpn_module.clients")
+local socket = require("socket")
 
 uloop.init()
 local conn = ubus.connect();
@@ -16,6 +15,49 @@ end
 
 local cursor = uci.cursor()
 local slist = {}
+
+local function send_cmd(host, port, cmd)
+    local tcp = assert(socket.tcp())
+    assert(tcp:connect(host, port))
+
+    tcp:send(cmd .. "\n")
+    local data = {}
+    while true do
+        local line = tcp:receive()
+        if not line or line:match("^END") then
+            break
+        end
+        if line:match("^SUCCESS") or line:match("^ERROR") then
+            data = {line}
+            break
+        end
+        table.insert(data, line)
+    end
+    tcp:close()
+    return table.concat(data, "\n")
+end
+
+local function parse_client_list(data)
+    local clients = {}
+    for line in data:gmatch("[^\n]+") do
+        if line:match("^CLIENT_LIST") then
+            local fields = {}
+            for field in line:gmatch("[^,]+") do
+                table.insert(fields, field)
+            end
+            local client = {
+                common_name = fields[2],
+                real_address = fields[3],
+                virtual_address = fields[4],
+                rx_bytes = tonumber(fields[5]),
+                tx_bytes = tonumber(fields[6]),
+                uptime = os.date("!%H:%M:%S", os.difftime(os.time(), tonumber(fields[8])))
+            }
+            clients[fields[2]] = client
+        end
+    end
+    return clients
+end
 
 cursor:foreach("openvpn", "openvpn", function(s)
     if s.name then
@@ -43,8 +85,8 @@ for _, server in ipairs(slist) do
     ovpn_methods["openvpn." .. server.name] = {
         clients = {
             function(req, msg)
-                local data = mgmt.send_cmd(server.mgmt_host, server.mgmt_port, "status 2")
-                local clients_list = clients.parse_client_list(data)
+                local data = send_cmd(server.mgmt_host, server.mgmt_port, "status 2")
+                local clients_list = parse_client_list(data)
                 conn:reply(req, clients_list)
             end, {}
         },
@@ -54,7 +96,7 @@ for _, server in ipairs(slist) do
                     conn:reply(req, {error = "Missing parameter"})
                     return
                 end
-                local data = mgmt.send_cmd(server.mgmt_host, server.mgmt_port, "kill " .. msg.name)
+                local data = send_cmd(server.mgmt_host, server.mgmt_port, "kill " .. msg.name)
                 conn:reply(req, {message = data})
             end, {name = ubus.STRING}
         },
